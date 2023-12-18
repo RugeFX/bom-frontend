73594caa84e5e6d663bf60bfb425d72bb0aa4c40
rugefx <zackfxg@gmail.com>
2023-12-18 20:12:18 +0700

    chore: delete placeholders

--- src/pages/inventory/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function InventoryPage() {
-  return (
-    <main className="space-y-4 p-8 pt-6">
-      <h1 className="text-3xl font-bold tracking-tight">Inventory</h1>
-    </main>
-  );
-}
